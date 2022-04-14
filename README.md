2d44a467af3b1b2e3d7a351d0f07bc21854eb958
Orion Aon <orion.aon@gmail.com>
2022-04-13 23:07:43 -0600

    add dice rolling api

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # D&D Character Generator
 
 A simple character generator for D&D using the Open 5e API
+
+Character assets created with Reroll.co App
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 fetch("https://api.open5e.com/races/")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
@@ -21,3 +23,15 @@ fetch("https://api.open5e.com/classes/")
     .catch(err => {
         console.log(`error ${err}`)
     });
+
+    fetch("http://roll.diceapi.com/json/4d6/")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        // dice rolling API set to roll 4d6, code in the total of the values minus the lowest and repeat 6 times, one for each attribute stat. Consider auto assigning the stats based on class type.
+        console.log(data)
+        // document.querySelector('#class').innerText = data.results[randomClass].name
+
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });  
